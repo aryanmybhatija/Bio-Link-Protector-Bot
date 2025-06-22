63ef6572163f8f195e166924bc2bfb80786b4a1d
aryanmybhatija <sonutomarlucky@gmail.com>
2025-06-22 13:55:24 +0530

    Update bio.py

--- bio.py
+++ bio.py
@@ -363,4 +363,5 @@ async def check_bio(client: Client, message):
         await reset_warnings(chat_id, user_id)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
